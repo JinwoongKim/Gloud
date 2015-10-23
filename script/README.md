eae7b04a50f9f5e60e7f6b36c49aff2b649d6fa2
JinwoongKim <aragnom@gmail.com>
2015-10-23 11:32:38 +0900

    Added README.md copy.sh

---
+++ script/README.md
@@ -0,0 +1,7 @@
+1) git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+2) sh copy.sh
+
+3) In vim editor, :PluginInstall
+
+
---
+++ script/copy.sh
@@ -0,0 +1,33 @@
+# copy vim files
+tar xvf vim.tar
+rm -rf ~/.vim
+mv .vim ~
+cp vimrc ~/.vimrc
+cp basic.vim ~/.basic.vim
+cp bundle.vim ~/.bundle.vim
+cp cscope.vim ~/.cscope.vim
+cp ctag.vim ~/.ctag.vim
+cp tab.vim ~/.tab.vim
+
+# copy git files
+cp gitconfig ~/.gitconfig
+
+# copy scripts
+mkdir ~/.scripts/
+cp mkcscope ~/.scripts/
+cp mkctags ~/.scripts/
+
+# zsh
+cp tmux.conf ~/.tmux.conf
+cp zshrc ~/.zshrc
+tar xvf tmux.tar
+rm -rf ~/.tmux
+mv .tmux ~
+tar xvf oh-my-zsh.tar
+rm -rf ~/.oh-my-zsh
+mv .oh-my-zsh ~
+
+
+
+
+
